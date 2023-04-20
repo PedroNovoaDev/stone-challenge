a4ee04e0b273c51fb7202b9b454c42a52425a959
Pedro Novoa <pedronovoa.21@gmail.com>
2023-04-20 13:46:15 -0300

    feat: setup basic express config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello there!');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
