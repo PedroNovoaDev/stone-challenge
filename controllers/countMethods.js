2237f8e43bce8c18983786beb9d46652c2123bcf
Pedro Novoa <pedronovoa.21@gmail.com>
2023-04-20 14:32:19 -0300

    feat: added increment ton visit count route

--- controllers/countMethods.js
+++ controllers/countMethods.js
@@ -1,8 +1,20 @@
 const axios = require('axios');
+const baseURL = 'https://api.countapi.xyz';
 
 module.exports = {
     showTonVisitCount: (req, res) => {
-        axios.get('https://api.countapi.xyz/get/ton.com.br')
+        axios.get(baseURL + '/get/ton.com.br')
+            .then(response => {
+                res.send(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                res.sendStatus(500);
+            });
+    },
+
+    incrementTonVisitCount: (req, res) => {
+        axios.get(baseURL + '/hit/ton.com.br')
             .then(response => {
                 res.send(response.data);
             })
--- routes/endpoints.js
+++ routes/endpoints.js
@@ -4,4 +4,6 @@ const countController = require('../controllers/countMethods');
 
 router.get('/show-ton-visit-count', countController.showTonVisitCount);
 
+router.get('/increment-ton-visit-count', countController.incrementTonVisitCount);
+
 module.exports = router;
