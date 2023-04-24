31c9d5815792bd93b7fe5b65467c679ba797e0f2
Pedro Novoa <pedronovoa.21@gmail.com>
2023-04-24 01:30:11 -0300

    feat: added token auth to count endpoints

--- controllers/countMethods.js
+++ controllers/countMethods.js
@@ -1,30 +1,56 @@
 const axios = require('axios');
 const baseURL = 'https://api.countapi.xyz';
+const auth = require('../lib/auth');
 
 module.exports = {
     showTonVisitCount: (req, res) => {
         // #swagger.tags = ['Count']
         // #swagger.description = Show Ton website visit count at the moment of the call'
-        axios.get(baseURL + '/get/ton.com.br')
-            .then(response => {
-                res.send(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-                res.sendStatus(500);
-            });
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+
+        try {
+            auth.verifyToken(token);
+
+            axios.get(baseURL + '/get/ton.com.br')
+                .then(response => {
+                    res.send(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.sendStatus(500);
+                });
+        } catch (error) {
+            console.log(error)
+            return res.status(401).json({ message: 'Invalid token' });
+        }
     },
 
     incrementTonVisitCount: (req, res) => {
         // #swagger.tags = ['Count']
         // #swagger.description = 'Increment Ton website visit count at the moment of the call.'
-        axios.get(baseURL + '/hit/ton.com.br')
-            .then(response => {
-                res.send(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-                res.sendStatus(500);
-            });
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+
+        try {
+            auth.verifyToken(token);
+
+            axios.get(baseURL + '/hit/ton.com.br')
+                .then(response => {
+                    res.send(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.sendStatus(500);
+                });
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
     }
 };
--- routes/endpoints.js
+++ routes/endpoints.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const countController = require('../controllers/countMethods');
 const usersController = require('../controllers/usersMethods');
 
-router.get('/show-ton-visit-count', countController.showTonVisitCount);
-
-router.get('/increment-ton-visit-count', countController.incrementTonVisitCount);
-
 router.post('/add-new-user', usersController.addNewUser);
 
 router.get('/search-user-by-id', usersController.searchUserById);
+
+router.get('/show-ton-visit-count', countController.showTonVisitCount);
+
+router.get('/increment-ton-visit-count', countController.incrementTonVisitCount);
 
 module.exports = router;
